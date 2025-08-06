90cbeff46da12dc7395bd3abc637dfa083edcc83
Prashant <iprashant2402@gmail.com>
2025-08-06 19:21:21 +0530

    UI Enhancement

--- src/modules/Auth/components/AuthDialog/AuthSignup.tsx
+++ src/modules/Auth/components/AuthDialog/AuthSignup.tsx
@@ -1,4 +1,6 @@
+import { Button } from "../../../../components/Button";
 import { AuthFormMode } from "./AuthDialog";
+import WelcomeCat from '../../../../assets/images/ImageWelcomeCat.svg?react';
 
 export const AuthSignup = (props: {
     errorMessage: string | null,
@@ -18,9 +20,18 @@ export const AuthSignup = (props: {
               </div>
             )}
 
+            <div className="auth-login-title">
+                <WelcomeCat height={150} width={150} color="var(--layout-text-color)" />
+                <div className="auth-login-title-text">
+                    <h3>Create your account</h3>
+                    <p>to sync your data across all your devices.</p>
+                </div>
+            </div>
+
+            <div className="auth-form-wrapper">
             <form onSubmit={handleSubmit} className="auth-form">
               <div className="auth-form-group">
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email" hidden>Email</label>
                 <input
                   type="email"
                   id="email"
@@ -35,7 +46,7 @@ export const AuthSignup = (props: {
               </div>
 
               <div className="auth-form-group">
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password" hidden>Password</label>
                 <input
                   type="password"
                   id="password"
@@ -50,18 +61,15 @@ export const AuthSignup = (props: {
               </div>
 
               <div className="auth-form-actions">
-                <button 
-                  type="submit" 
-                  className="auth-form-submit"
+                <Button
+                  variant="primary"
+                  type="submit"
+                  label={isLoading ? 'Creating account...' : 'Create account'}
                   disabled={isLoading}
-                >
-                  {isLoading 
-                    ? ('Creating account...') 
-                    : ('Create Account')
-                  }
-                </button>
+                />
               </div>
             </form>
+            </div>
         <div className="auth-form-toggle">
               <p>
                 Already have an account?
